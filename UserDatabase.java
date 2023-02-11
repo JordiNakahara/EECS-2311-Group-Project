b448bd382660538918007b7e4bab45b956d40b12
Jordi <jordi13@my.yorku.ca>
2023-02-11 10:34:55 -0500

    Added methods to read and return individual properties of the user from the database

--- UserDatabase.java
+++ UserDatabase.java
@@ -26,21 +26,153 @@ public class UserDatabase {
 		}
 
 	}
-	
+
 	public static String readUserFile() {
 		String output = "";
 		File userInfo = new File("userInfo.txt");
-		
+
 		try {
 			Scanner sc = new Scanner(userInfo);
-			while(sc.hasNextLine()) {
+			while (sc.hasNextLine()) {
 				output = output + "\n" + sc.nextLine();
 			}
+			sc.close();
+		} catch (Exception e) {
+
+		}
+
+		return output;
+	}
+
+	public static String readFirstName() {
+		String output = "";
+		File userInfo = new File("userInfo.txt");
+
+		try {
+			Scanner sc = new Scanner(userInfo);
+			output = output + sc.nextLine();
+			sc.close();
 		} catch (Exception e) {
 			
 		}
 		
+		int temp = output.indexOf(':') + 1;
+		output = output.substring(temp);
 		return output;
+
+	}
+
+	
+	public static String readLastName() {
+		String output = "";
+		File userInfo = new File("userInfo.txt");
+
+		try {
+			Scanner sc = new Scanner(userInfo);
+			sc.nextLine();
+			output = output + "\n" + sc.nextLine();
+			sc.close();
+		} catch (Exception e) {
+			
+		}
+		
+		int temp = output.indexOf(':') + 1;
+		output = output.substring(temp);
+		return output;
+
+	}
+	
+	public static char readGender() {
+		char output = 'e';
+		String input = "";
+		File userInfo = new File("userInfo.txt");
+
+		try {
+			Scanner sc = new Scanner(userInfo);
+			sc.nextLine();
+			sc.nextLine();
+			input = sc.nextLine();
+			sc.close();
+		} catch (Exception e) {
+			
+		}
+		
+		int temp = input.indexOf(':') + 1;
+		output = input.charAt(temp);
+		return output;
+
+	}
+	
+	public static int readWeight() {
+		int output = 0;
+		String input = "";
+		File userInfo = new File("userInfo.txt");
+
+		try {
+			Scanner sc = new Scanner(userInfo);
+			sc.nextLine();
+			sc.nextLine();
+			sc.nextLine();
+			input = sc.nextLine();
+			sc.close();
+		} catch (Exception e) {
+			
+		}
+		
+		int temp = input.indexOf(':') + 1;
+		input = input.substring(temp);
+		output = Integer.parseInt(input);
+		return output;
+
+	}
+	
+	public static int readHeight() {
+		int output = 0;
+		String input = "";
+		File userInfo = new File("userInfo.txt");
+
+		try {
+			Scanner sc = new Scanner(userInfo);
+			sc.nextLine();
+			sc.nextLine();
+			sc.nextLine();
+			sc.nextLine();
+			input = sc.nextLine();
+			sc.close();
+		} catch (Exception e) {
+			
+		}
+		
+		int temp = input.indexOf(':') + 1;
+		input = input.substring(temp);
+		output = Integer.parseInt(input);
+		return output;
+
+	}
+	
+	public static int readAge() {
+		int output = 0;
+		String input = "";
+		File userInfo = new File("userInfo.txt");
+
+		try {
+			Scanner sc = new Scanner(userInfo);
+			sc.nextLine();
+			sc.nextLine();
+			sc.nextLine();
+			sc.nextLine();
+			sc.nextLine();
+			input = sc.nextLine();
+			sc.close();
+		} catch (Exception e) {
+			
+		}
+		
+		int temp = input.indexOf(':') + 1;
+		input = input.substring(temp);
+		output = Integer.parseInt(input);
+		return output;
+
 	}
 
 }
