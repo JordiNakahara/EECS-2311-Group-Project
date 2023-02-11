819c2a4a0cd465f50ca360d215ed3560064a0ccf
Jordi <jordi13@my.yorku.ca>
2023-02-10 19:51:32 -0500

    Removed test portions and added functionality to read from text file

--- UserDatabase.java
+++ UserDatabase.java
@@ -1,37 +1,48 @@
+package EECS2311;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
 
-
 public class UserDatabase {
-	
-	
+
 	public static void writeUserToFile(User user) {
-		String output = "FirstName:" + user.getFName() + "\nLastName:" + user.getLName() + 
-				"\nGender:" + user.getGender() + "\nWeight:" + user.getWeight()
-				+ "\nHeight:" + user.getHeight() + "\nAge:" + user.getAge()
-				+ "\nBMR:" + user.getBMR();
+		String output = "FirstName:" + user.getFName() + "\nLastName:" + user.getLName() + "\nGender:"
+				+ user.getGender() + "\nWeight:" + user.getWeight() + "\nHeight:" + user.getHeight() + "\nAge:"
+				+ user.getAge() + "\nBMR:" + user.getBMR();
+		File userInfo = new File("userInfo.txt");
 		try {
-			File userInfo = new File("userInfo.txt");
-			
-			if(userInfo.createNewFile()) {
+
+			if (userInfo.createNewFile()) {
 				FileWriter writer = new FileWriter("userInfo.txt");
 				writer.write(output);
 				writer.close();
-				System.out.println("Tried to write stuff");
-				
-			}
-			else {
+
+			} else {
 				FileWriter writer = new FileWriter("userInfo.txt");
 				writer.write(output);
 				writer.close();
-				System.out.println("Tried to write stuff. File already exists");
 			}
+		} catch (Exception e) {
+			System.out.println("Something went wrong while trying to write to the file.");
 		}
-		catch (Exception e) {
+
+	}
+	
+	public static String readUserFile() {
+		String output = "";
+		File userInfo = new File("userInfo.txt");
+		
+		try {
+			Scanner sc = new Scanner(userInfo);
+			while(sc.hasNextLine()) {
+				output = output + "\n" + sc.nextLine();
+			}
+		} catch (Exception e) {
 			
 		}
 		
+		return output;
 	}
-	
+
 }
