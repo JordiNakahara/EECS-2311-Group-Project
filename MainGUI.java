56ecd124598af438a8101496ae7a53baee1355ff
HarrishElango <58488367+HarrishElango@users.noreply.github.com>
2023-03-07 15:35:34 -0500

    Updated MainGUI to include further stages
    Reverted MainGUI to improved version of initial GUI created by Harjot and modified by by Gaurav to include workout start page

--- MainGUI.java
+++ MainGUI.java
@@ -1,57 +1,53 @@
-package eecs2311gui;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class MainGUI extends Application {
 
-   
+    @Override
     public void start(Stage primaryStage) {
-	// Create the buttons
-	Button userProfileBtn = new Button("User Profile");
-	Button workoutsBtn = new Button("Workouts");
+        // Create the buttons
+        Button userProfileBtn = new Button("User Profile");
+        Button workoutsBtn = new Button("Workouts");
 
-	// Attach event handlers to the buttons
-	userProfileBtn.setOnAction(e -> {
-	    // Create a new window for the user profile GUI
-	    Stage userProfileStage = new Stage();
-	    UserProfileGUI userProfileGUI = new UserProfileGUI();
-	    userProfileGUI.start(userProfileStage);
-	    primaryStage.close();
-	});
+        // Attach event handlers to the buttons
+        userProfileBtn.setOnAction(e -> {
+            // Create a new window for the user profile GUI
+            Stage userProfileStage = new Stage();
+            UserProfileGUI userProfileGUI = new UserProfileGUI();
+            userProfileGUI.start(userProfileStage);
+            primaryStage.close();
+        });
 
-	workoutsBtn.setOnAction(e -> {
-	    // Create a new window for the workouts GUI
-	    Stage workoutsStage = new Stage();
-	    WorkoutsGUI workoutsGUI = new WorkoutsGUI();
-	    workoutsGUI.start(workoutsStage);
-	    primaryStage.close();
-	});
+        workoutsBtn.setOnAction(e -> {
+            // Create a new window for the workouts GUI
+            Stage workoutsStage = new Stage();
+            WorkoutsGUI workoutsGUI = new WorkoutsGUI();
+            workoutsGUI.start(workoutsStage);
+            primaryStage.close();
+        });
 
-	// Create a vertical box and add the buttons to it
-	VBox root = new VBox(20);
-	root.setAlignment(Pos.CENTER);
-	root.getChildren().addAll(userProfileBtn, workoutsBtn);
+        // Create a vertical box and add the buttons to it
+        VBox root = new VBox(20);
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(userProfileBtn, workoutsBtn);
 
-	// Create a scene and set the root node
-	Scene scene = new Scene(root, 300, 200);
+        // Create a scene and set the root node
+        Scene scene = new Scene(root, 600, 400);
 
-	// Set the stage's title and scene, and show the stage
-	primaryStage.setTitle("Main GUI");
-	primaryStage.setScene(scene);
-	primaryStage.show();
+        // Set the stage's title and scene, and show the stage
+        primaryStage.setTitle("Main GUI");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-	launch(args);
+        launch(args);
     }
 }
 
@@ -59,29 +55,30 @@ class UserProfileGUI extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-	// Create the user profile GUI
-	VBox root = new VBox();
-	Button backButton = new Button("Back");
+        // Create the user profile GUI
+        VBox root = new VBox(20);
+        Button backButton = new Button("Back");
 
-	backButton.setOnAction(e -> {
-	    // Create a new window for the user profile GUI
-	    Stage userProfileStage = new Stage();
-	    MainGUI mainGUI = new MainGUI();
-	    mainGUI.start(userProfileStage);
-	    primaryStage.close();
-	});
+        TextField field = new TextField("name");
 
-	root.setAlignment(Pos.TOP_LEFT);
-	root.getChildren().addAll(backButton);
+        backButton.setOnAction(e -> {
+            // Create a new window for the user profile GUI
+            Stage userProfileStage = new Stage();
+            MainGUI mainGUI = new MainGUI();
+            mainGUI.start(userProfileStage);
+            primaryStage.close();
+        });
 
-	// Create a scene and set the root node
-	Scene scene = new Scene(root, 300, 200);
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(field, backButton);
 
-	// Set the stage's title and scene, and show the stage
-	primaryStage.setTitle("User Profile");
-	primaryStage.setScene(scene);
-	primaryStage.show();
+        // Create a scene and set the root node
+        Scene scene = new Scene(root, 600, 400);
 
+        // Set the stage's title and scene, and show the stage
+        primaryStage.setTitle("User Profile");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
@@ -90,46 +87,141 @@ class WorkoutsGUI extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-	// Making the workouts GUI primnary page
-	BorderPane root = new BorderPane();
-	root.setPadding(new Insets(20));
-	
-	// making the top HBox for the title and buttons
-	HBox topBox = new HBox(10);
-	topBox.setAlignment(Pos.CENTER);
-	topBox.setPadding(new Insets(10));
-	
-	// Creating the title label
-	Label titleLabel = new Label("Workouts");
-	titleLabel.setFont(Font.font("Arial", 24));
-	
-	// making the buttons for add, edit and delete workout.
-	Button addBtn = new Button("Add Workout");
-	Button editBtn = new Button("Edit Workout");
-	Button deleteBtn = new Button("Delete Workout");
-	
-	// Add the buttons to the top HBox
-	topBox.getChildren().addAll(titleLabel, addBtn, editBtn, deleteBtn);
-	
-	// Create the center VBox for the list of workouts
-	VBox centerBox = new VBox(10);
-	centerBox.setAlignment(Pos.CENTER);
-	centerBox.setPadding(new Insets(10));
-	
-	// Still need to add list of workouts to the center VBox
-	
-	// Connecting the top HBox and center VBox to the root BorderPane
-	root.setTop(topBox);
-	root.setCenter(centerBox);
-	
-	// Making a scene and set the root node
-	Scene scene = new Scene(root, 600, 600);
-	
-	// Setting the stage's title and scene, and showing the stage
-	primaryStage.setTitle("Workouts");
-	primaryStage.setScene(scene);
-	primaryStage.show();
+        // Create the workouts GUI
+        VBox root = new VBox(20);
+        HBox foot = new HBox();
+        Button backButton = new Button("Back");
+        Button chestButton = new Button("Chest");
+        Button armsButton = new Button("Arms");
+        Button absButton = new Button("Abs");
+        Button legsButton = new Button("Legs");
+        Button cardioButton = new Button("Cardio");
+        // Button comboButton = new Button("Combo");
+
+        backButton.setOnAction(e -> {
+            // Create a new window for the user profile GUI
+            Stage welcomeStage = new Stage();
+            MainGUI mainGUI = new MainGUI();
+            mainGUI.start(welcomeStage);
+            primaryStage.close();
+        });
+
+        chestButton.setOnAction(e -> {
+            // Create a new window for the user profile GUI
+            Stage chestStage = new Stage();
+            WorkoutStartGUI testGUI = new WorkoutStartGUI();
+            testGUI.type = "Chest";
+            testGUI.start(chestStage);
+            primaryStage.close();
+        });
+
+        armsButton.setOnAction(e -> {
+            // Create a new window for the user profile GUI
+            Stage armStage = new Stage();
+            WorkoutStartGUI testGUI = new WorkoutStartGUI();
+            testGUI.type = "Arms";
+            testGUI.start(armStage);
+            primaryStage.close();
+        });
+
+        absButton.setOnAction(e -> {
+            // Create a new window for the user profile GUI
+            Stage abStage = new Stage();
+            WorkoutStartGUI testGUI = new WorkoutStartGUI();
+            testGUI.type = "Abs";
+            testGUI.start(abStage);
+            primaryStage.close();
+        });
+
+        legsButton.setOnAction(e -> {
+            // Create a new window for the user profile GUI
+            Stage legStage = new Stage();
+            WorkoutStartGUI testGUI = new WorkoutStartGUI();
+            testGUI.type = "Legs";
+            testGUI.start(legStage);
+            primaryStage.close();
+        });
+
+        cardioButton.setOnAction(e -> {
+            // Create a new window for the user profile GUI
+            Stage cardioStage = new Stage();
+            WorkoutStartGUI testGUI = new WorkoutStartGUI();
+            testGUI.type = "Cardio";
+            testGUI.start(cardioStage);
+            primaryStage.close();
+        });
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(chestButton, armsButton, absButton, legsButton, cardioButton, backButton);
+
+        // Create a scene and set the root node
+        Scene scene = new Scene(root, 600, 400);
+
+        // Set the stage's title and scene, and show the stage
+        primaryStage.setTitle("Workouts");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
-// Work in progress, using JavaFx to attempt to make GUI
-// Works using IntelliJ Idea, ensure the "Modules Info is updated"
+
+class WorkoutStartGUI extends Application {
+
+    public void start(Stage primaryStage) {
+
+        VBox root = new VBox(20);
+        ComboBox combobox = new ComboBox();
+        combobox.getItems().add("Choice 1");
+        combobox.getItems().add("Choice 2");
+        combobox.getItems().add("Choice 3");
+        combobox.getSelectionModel().selectFirst();
+
+        ComboBox combobox1 = new ComboBox();
+        combobox1.getItems().add("Choice 1");
+        combobox1.getItems().add("Choice 2");
+        combobox1.getItems().add("Choice 3");
+        combobox1.getSelectionModel().selectFirst();
+
+        ComboBox combobox2 = new ComboBox();
+        combobox2.getItems().add("Choice 1");
+        combobox2.getItems().add("Choice 2");
+        combobox2.getItems().add("Choice 3");
+        combobox2.getSelectionModel().selectFirst();
+	    
+	// Will need to figure out how to connect these combo boxes
+
+        Button nextButton = new Button("Continue");
+        Button backButton = new Button("Back");
+
+        backButton.setOnAction(e -> {
+            // Create a new window for the user profile GUI
+            Stage workoutsStage = new Stage();
+            WorkoutsGUI workoutsGUI = new WorkoutsGUI();
+            workoutsGUI.start(workoutsStage);
+            primaryStage.close();
+        });
+
+        nextButton.setOnAction(e -> {
+            // Create a new window for the user profile GUI
+            Stage difficultyStage = new Stage();
+            TimerSet timerset = new TimerSet();
+            timerset.start(difficultyStage);
+            primaryStage.close();
+        });
+
+
+        root.getChildren().addAll(combobox, combobox1, combobox2,nextButton, backButton);
+        root.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(root, 600, 400);
+
+        primaryStage.setTitle("Workout Start");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+
+
+}
+
+
+// to get this to work, you need to have the javafx libraries included and make sure that you set up the VM options to use those libraries
